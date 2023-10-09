1ae572fc415ba8981bce25e4e76796af4099dfeb
mohamed nasser <m.nasser.15910@gmail.com>
2023-10-09 14:16:24 +0300

    updated fixcal to use path and node not ts-node

--- messages/messagesKeys.ts
+++ messages/messagesKeys.ts
@@ -1,4 +1,4 @@
 export const navlinksKeys = ["/","permenant-immigration","visit","study","blog","about-us","book-consult","assessment"];
-export const signinFormKeys = ["email","password","forgotPassword","signin"];
-export const signupFormKeys = ["firstName","lastName","dateOfBirth","email","password","confirmPassword","signup"];
-export const commonKeys = ["signin","signup"];
+export const signinFormKeys = ["email","password","forgotPassword","signin","signup","don'tHaveAnAccount"];
+export const signupFormKeys = ["firstName","lastName","dateOfBirth","email","password","confirmPassword","signin","signup","alreadyAMember"];
+export const commonKeys = ["signin","signup","signout"];
--- src/utils/other/fixCal.js
+++ src/utils/other/fixCal.js
@@ -1,20 +1,20 @@
 // fix-type.ts
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require("fs");
+const path = require("path");
+
+const absolutePath = path.resolve(
+  __dirname,
+  "../../../node_modules/@calcom/embed-react/package.json"
+);
 
 function fixType() {
-  const packageJsonRaw = fs.readFileSync(
-    "../../../node_modules/@calcom/embed-react/package.json",
-    "utf8"
-  );
+  const packageJsonRaw = fs.readFileSync(absolutePath, "utf8");
   const packageJson = JSON.parse(packageJsonRaw);
 
   packageJson.exports["."].types = "./dist/embed-react/src/index.d.ts";
 
-  fs.writeFileSync(
-    "../../../node_modules/@calcom/embed-react/package.json",
-    JSON.stringify(packageJson, null, 2)
-  );
+  fs.writeFileSync(absolutePath, JSON.stringify(packageJson, null, 2));
 }
 
 fixType();
