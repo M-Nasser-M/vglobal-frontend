de1c5a89b6217887048cd8085f6764ba8a21d9be
mohamed nasser <m.nasser.15910@gmail.com>
2024-01-06 11:52:51 +0200

    fix: params locale ResendConfirmation

--- src/app/[locale]/resend-confirmation-mail/page.tsx
+++ src/app/[locale]/resend-confirmation-mail/page.tsx
@@ -1,13 +1,14 @@
-import type { Locale } from "@/i18n";
 import ResendConfirmationMailForm from "./ResendConfirmationMailForm";
 import { unstable_setRequestLocale } from "next-intl/server";
+import type { Locale } from "@/i18n";
 
 type Props = {
-  locale: Locale;
+  params: { locale: Locale };
 };
-const page = ({ locale }: Props) => {
+
+const Page = ({ params: { locale } }: Props) => {
   unstable_setRequestLocale(locale);
   return <ResendConfirmationMailForm />;
 };
 
-export default page;
+export default Page;
