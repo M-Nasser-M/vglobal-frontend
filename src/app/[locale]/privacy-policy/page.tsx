a7150531f63fa8b246fd9aecb59e1497136a6ea8
mohamed nasser <m.nasser.15910@gmail.com>
2024-01-14 05:37:04 +0200

    fix add seo to privacy pagfe

--- src/app/[locale]/privacy-policy/page.tsx
+++ src/app/[locale]/privacy-policy/page.tsx
@@ -1,17 +1,37 @@
 import HtmlContentWrapper from "@/components/HtmlConntentWrapper";
 import NoContent from "@/components/NoContent";
 import type { Locale } from "@/i18n";
+import { getOpenGraph, getTwitter } from "@/utils/other/utils";
 import { getPrivacyPolicyArticleAndSeoo } from "@/utils/services/privacyPolicy";
 import { ArticleAndSeoSchema } from "@/utils/types/articleAndSeoTypes";
+import type { Metadata } from "next";
 import { unstable_setRequestLocale } from "next-intl/server";
 
 type Props = { params: { locale: Locale } };
 
-const Page = async ({ params }: Props) => {
-  unstable_setRequestLocale(params.locale);
-  const lang = params.locale;
+export async function generateMetadata({
+  params: { locale },
+}: Props): Promise<Metadata> {
+  const response = await getPrivacyPolicyArticleAndSeoo(locale);
+  const seo = response?.data.seo;
+  const twitter = seo?.metaSocial && getTwitter(seo.metaSocial);
+  const openGraph = seo?.metaSocial && getOpenGraph(seo.metaSocial);
+  return {
+    metadataBase: new URL("https://www.vglobal.ca"),
+    title: seo?.metaTitle,
+    description: seo?.metaDescription,
+    alternates: { canonical: seo?.canonicalURL },
+    robots: seo?.metaRobots,
+    keywords: seo?.keywords,
+    twitter: twitter,
+    openGraph: openGraph,
+  };
+}
 
-  const response = await getPrivacyPolicyArticleAndSeoo(lang);
+const Page = async ({ params: { locale } }: Props) => {
+  unstable_setRequestLocale(locale);
+
+  const response = await getPrivacyPolicyArticleAndSeoo(locale);
 
   const validateData = ArticleAndSeoSchema.safeParse(response);
 
--- src/utils/services/privacyPolicy.ts
+++ src/utils/services/privacyPolicy.ts
@@ -1,11 +1,14 @@
 import { serverApiAuth } from "./fetchApiServer";
 import { Locale } from "@/i18n";
 import qs from "qs";
+import type { ArticleAndSeo } from "../types/articleAndSeoTypes";
 
 export const getPrivacyPolicyArticleAndSeoo = async (locale: Locale) => {
   const query = qs.stringify({ locale });
   try {
-    const response = await serverApiAuth.get(`/privacy-policy?${query}`);
+    const response = await serverApiAuth.get<ArticleAndSeo>(
+      `/privacy-policy?${query}`
+    );
 
     return response;
   } catch (error) {
